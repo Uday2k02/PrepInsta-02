a79ccb157f94d0d3bad55354e04e2d330c6d6525
Kavali Uday Kumar <126236577+Uday2k02@users.noreply.github.com>
2024-07-22 22:19:47 +0530

    Delete JS_tasks directory

--- JS_tasks/task2.js
+++
@@ -1,7 +0,0 @@
-function calculatePetAge(age, conversion_age = 7){
-    years = age * conversion_age;
-    console.log(`Your kitty is ${years} years old in cat years`);
-}
-calculatePetAge(2);
-calculatePetAge(3);
-calculatePetAge(1);
